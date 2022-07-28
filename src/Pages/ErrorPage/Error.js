d31b0418ae626f8771c59dbb5a7f22eeace2d602
ashrafur22 <ashrafurrahman22@gmail.com>
2022-07-28 08:25:42 +0600

    error-page-responsive

--- src/Pages/ErrorPage/Error.js
+++ src/Pages/ErrorPage/Error.js
@@ -4,8 +4,8 @@ const Error = () => {
     return (
         <div style={{fontFamily:"Inter", letterSpacing:"2px"}} className='flex justify-center min-h-screen items-center'>
             <div className='flex flex-col text-center'>
-            <h1 style={{color:"#D0F344"}} className='text-9xl font-semibold'>404!</h1>
-            <h1 style={{color:"#28ACE2"}} className='text-5xl font-semibold'>Sorry! Page Not Found.</h1>
+            <h1 style={{color:"#D0F344"}} className='lg:text-9xl text-4xl font-semibold'>404!</h1>
+            <h1 style={{color:"#28ACE2"}} className='lg:text-5xl text-3xl font-semibold'>Sorry! Page Not Found.</h1>
             </div>
         </div>
     );
